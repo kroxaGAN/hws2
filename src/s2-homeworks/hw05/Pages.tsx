bacb69e3549ddfa4a7d9a708009c577a7f9cdee2
Andrei Hutsau <mwplt@mail.ru>
2022-12-02 22:33:57 +0300

    tuesday 1 route and bad style

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -16,16 +16,20 @@ function Pages() {
         <div>
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
+
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
-
+                {/*<Route  path={'...'} element={<Navigate to={...} />}*/}
+                <Route  path='/' element={<Navigate to={PATH.PRE_JUNIOR}/>} />
+aa
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior />}/>
+                <Route path={PATH.JUNIOR} element={<Junior />}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />}/>
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+                <Route path={'*'} element={<Navigate to={'/404'}/>}/>
+                <Route path={'/404'} element={<Error404/>}/>
                 {/*<Route ...*/}
             </Routes>
         </div>
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,4 +1,3 @@
-import {UserType} from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
@@ -7,11 +6,11 @@ type ActionType =
 export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
+            // sort() создаёт новый массив? или нужно в ручную?...
             return state // need to fix
         }
         case 'check': {
-
+            // filter() создаёт новый массив? или нужно в ручную?...
             return state // need to fix
         }
         default:
