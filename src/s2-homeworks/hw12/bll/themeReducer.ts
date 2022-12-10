cc1bca8794d1bc6f35ec091547d37e2c1e90503e
Andrei Hutsau <mwplt@mail.ru>
2022-12-10 23:31:39 +0300

    wensday 4 made change select

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,20 @@
 const initState = {
     themeId: 1,
 }
+type statePropsType={
+    themeId: number
+}
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state:statePropsType = initState, action: changeThemeIdType): statePropsType => { // fix any
     switch (action.type) {
         // дописать
-
+        case "SET_THEME_ID":{
+            return {...state, themeId:action.id}
+        }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type changeThemeIdType=ReturnType<typeof changeThemeId>
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id } as const) // fix any
